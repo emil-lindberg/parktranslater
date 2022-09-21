cdf0c63048464968252bb26904f2f02df5422522
emil-lindberg <emil.lindberg090@gmail.com>
2022-09-21 12:30:22 +0200

    git commit -am.

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,33 @@ class AppBarMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(centerTitle: true, title: const Text('ParkTranslater')));
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Park Translater'),
+      ),
+      body: Center(
+          child: IconButton(
+        icon: const Icon(Icons.camera_alt_rounded),
+        iconSize: 75,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ParkCamera()),
+          );
+        },
+      )),
+    );
+  }
+}
+
+class ParkCamera extends StatelessWidget {
+  const ParkCamera({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Park Scanner'),
+      ),
+    );
   }
 }
