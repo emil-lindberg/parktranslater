697ef2cb0d0ad2ee1beb722e0c95a564e89d0a18
emil-lindberg <emil.lindberg090@gmail.com>
2022-09-28 11:31:25 +0200

    git commit am.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(const AppBarApp());
 
@@ -13,9 +16,17 @@ class AppBarApp extends StatelessWidget {
   }
 }
 
-class AppBarMain extends StatelessWidget {
+class AppBarMain extends StatefulWidget {
   const AppBarMain({super.key});
+
   @override
+  State<AppBarMain> createState() => _AppBarMainState();
+}
+
+class _AppBarMainState extends State<AppBarMain> {
+  @override
+  State<AppBarMain> createState() => _AppBarMainState();
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -37,39 +48,69 @@ class AppBarMain extends StatelessWidget {
   }
 }
 
-class ParkCamera extends StatelessWidget {
+class ParkCamera extends StatefulWidget {
   const ParkCamera({super.key});
+
+  @override
+  State<ParkCamera> createState() => _ParkCameraState();
+}
+
+class _ParkCameraState extends State<ParkCamera> {
   @override
   Widget build(BuildContext context) {
+    File? imageFile;
     return Scaffold(
         appBar: AppBar(
           title: const Text('Park Scanner'),
         ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: [
-              SizedBox(
-                  width: 175,
-                  height: 75,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        textStyle: const TextStyle(fontSize: 40)),
-                    child: const Text('knapp'),
-                  )),
-              SizedBox(
-                  width: 175,
-                  height: 75,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        textStyle: const TextStyle(fontSize: 40)),
-                    child: const Text('knopp'),
-                  )),
+              Container(
+                width: 640,
+                height: 480,
+                alignment: Alignment.center,
+                child: const Text('Image will appear here'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                      width: 175,
+                      height: 75,
+                      child: ElevatedButton(
+                        onPressed: () => fetchImage(source: ImageSource.camera),
+                        style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 30)),
+                        child: const Text('Capture Picture'),
+                      )),
+                  SizedBox(
+                      width: 175,
+                      height: 75,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 30)),
+                        child: const Text('Select Picture'),
+                      )),
+                ],
+              )
             ],
           ),
         ));
+  }
+
+  void fetchImage({required ImageSource source, imageFile}) async {
+    final file = await ImagePicker().pickImage(source: source);
+
+    if (file?.path != null) {
+      setState(() {
+        imageFile = File(file!.path);
+      });
+    }
   }
 }
